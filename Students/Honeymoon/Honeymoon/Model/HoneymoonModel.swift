1a7ecc2dbb31f866425828d581152db2310984c1
kidflash2jahaan <53150988+kidflash2jahaan@users.noreply.github.com>
2022-06-21 17:23:05 -0700

    beta card view

---
+++ Students/Honeymoon/Honeymoon/Model/HoneymoonModel.swift
@@ -0,0 +1,14 @@
+//
+//  HoneymoonModel.swift
+//  Honeymoon
+//
+//  Created by Jahaan Pardhanani on 6/21/22.
+//
+
+import SwiftUI
+
+struct Destination {
+    var place: String
+    var country: String
+    var image: String
+}
