cc7188143de79252c09e79957c2fd5cad8d1feec
kidflash2jahaan <53150988+kidflash2jahaan@users.noreply.github.com>
2022-06-22 12:58:59 -0700

    beta multiple cards

--- Students/Honeymoon/Honeymoon/Views/ContentView.swift
+++ Students/Honeymoon/Honeymoon/Views/ContentView.swift
@@ -11,14 +11,34 @@ struct ContentView: View {
     @State var showAlert: Bool = false
     @State var showGuide: Bool = false
     
+    var cardViews: [CardView] = {
+        var views = [CardView]()
+        for index in 0..<2 {
+            views.append(CardView(honeymoon: honeymoonData[index]))
+        }
+        return views
+    }()
+    
+    private func isTopCard(cardView: CardView) -> Bool {
+        guard let index = cardViews.firstIndex(where: {$0.id == cardView.id}) else {
+            return false
+        }
+        return index == 0
+    }
+    
     var body: some View {
         VStack {
             HeaderView(showGuideView: $showGuide)
             
             Spacer()
             
-            CardView(honeymoon: honeymoonData[2])
-                .padding()
+            ZStack {
+                ForEach(cardViews) {cardView in
+                    cardView
+                        .zIndex(self.isTopCard(cardView: cardView) ? 1 : 0)
+                }
+            }
+            .padding(.horizontal)
             
             Spacer()
             
